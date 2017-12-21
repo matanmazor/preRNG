a18ab01fec5c9ad45d3bd06afe72b98b77c1abcd
Matan <matanmazor@outlook.com>
2017-12-21 13:51:33 +0000

    adaptations for PyPi2

--- Python/prerng/setup.py
+++ Python/prerng/setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
-setup(
-     name='preRNG',    
-     version='0.1',                          
-     scripts=['preRNG']                  
-)
+setup(name='prerng',
+      version='0.1',
+      description='in-lab time-locking of study preregistration',
+      url='https://github.com/matanmazor/preRNG/tree/master/Python/prerng',
+      author='Mazor Bros',
+      author_email='matanmazor@outlook.com',
+      license='3 clause bsd',
+      packages=['preRNG'],
+      zip_safe=False)
